8f592cede9926bcd03c3acb53c6e6077b8944fd1
周伟 <zhouwei@techfuser.net>
2020-12-14 14:32:41 +0800

    first commit

---
+++ src/main/java/com/wenxiahy/hy/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.wenxiahy.hy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author zhouw
+ * @Description
+ * @Date 2020-12-14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
